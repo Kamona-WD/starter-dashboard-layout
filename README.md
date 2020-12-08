cd7cf8314027552045d984101f99a447890fa1ee
kamona <ak3849909@gmail.com>
2020-12-09 00:32:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Starter Responsive Dashboard Layout
+
+Starter responsive dashboard layout built with tailwindcss alpinejs
+
+- tailwindcss 2.0.1
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: ['./public/**/*.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    overflow: ['hover'],
+  },
+  plugins: [],
+}
